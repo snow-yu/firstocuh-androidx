38e0949aedd1dde950f99653fa4b1e05272934eb
jiaxue.yu <197638651@qq.com>
2021-02-20 13:13:57 +0800

    feat：新增jitpack发布配置

--- library_base/src/main/java/com/firstouch/library/base/BaseApp.kt
+++ library_base/src/main/java/com/firstouch/library/base/BaseApp.kt
@@ -11,4 +11,13 @@ import android.app.Application
  */
 class BaseApp: Application() {
 
+    companion object {
+        lateinit var instance: BaseApp
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
 }
--- library_base/src/main/java/com/firstouch/library/base/activity/BaseActivity.kt
+++ library_base/src/main/java/com/firstouch/library/base/activity/BaseActivity.kt
@@ -14,5 +14,4 @@ abstract class BaseActivity(@LayoutRes contentLayoutId: Int) : AppCompatActivity
 
 
 
-
 }
